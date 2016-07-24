70d7104312303a5c0a883e8c2d0018ea4d3dd5ad
Muhammad Rizwan <rizwan.muet@gmail.com>
2016-07-24 22:57:47 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/CrossOverTest/';
+$config['base_url'] = 'http://cicross-rizwani.rhcloud.com/';
 
 /*
 |--------------------------------------------------------------------------
