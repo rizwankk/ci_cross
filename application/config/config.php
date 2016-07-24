6a7589d8e260f1579cfabc3061bd6cf1ae0c7f7d
Muhammad Rizwan <rizwan.muet@gmail.com>
2016-07-24 23:54:10 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://' . getenv('OPENSHIFT_APP_DNS') . '/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
