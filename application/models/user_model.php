142a127584be98b4e7391ee44caca4ea33f8585b
Muhammad Rizwan <rizwan.muet@gmail.com>
2016-07-25 00:29:02 +0500

    Update user_model.php

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class User_model extends CI_Model {
+class user_model extends CI_Model {
 
     public function __construct()
     {
